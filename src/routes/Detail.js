b1f5032310f45fa5a962001f286ac308b570fff8
bgyuee <pky5396@gmail.com>
2023-03-09 10:26:57 +0900

    home edit

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import '../styles/Detail.css'
 
 // 주소창에 전달되는 프랍스를 받을라면은 useLocation()이라는 함수를 써야된다
@@ -8,6 +8,10 @@ function Detail() {
   console.log(location);
   const {genres,poster,summary,title,year} = location.state;
   // 데이터를 구조분해 할당으로 받아와서 밑에처럼 다시 뿌려준다 loacation객체에 기본적으로 state라는 속성이 있어서 state를 써준다.
+
+  const navigate = useNavigate();
+  if(location.state === undefined){} //만약에 그냥 주소창에 치고 들어오면은 전달된 props값이 없다 그럴경우에는 navigate함수를 쓰자
+    navigate('/'); // 홈으로 이동 즉 리다이렉트 기능이다.
 
   return (
     <div className='detail'>
--- src/routes/Home copy.js
+++ src/routes/Home copy.js
@@ -1,30 +1,22 @@
 // 클래스형 컴퍼넌트
 import axios from 'axios';
-import React, { Component, useEffect, useState } from 'react'
+import React, { Component } from 'react'
 import Movie from '../components/Movie';
 import '../styles/Home.css';
 
-function Home() {
-  // state = {
-  //   isLoading: true,
-  //   movies: [],
-  // } 이거 대신에 함수형으로 바꾸면 밑에서처럼 useState()라는 훅함수를 사용해준다!!
+export class Home extends Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  }
+componentDidMount() {
+  // setTimeout(() => { //6초지나면 isLoading을 flase로 바꿔라
+  //   this.setState({isLoading:false});
+  // }, 6000);
+  this.getMovies();
+}
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies,setMovies] = useState([]);
-
-// componentDidMount() {
-//   // setTimeout(() => { //6초지나면 isLoading을 flase로 바꿔라
-//   //   this.setState({isLoading:false});
-//   // }, 6000);
-//   this.getMovies();
-// } 밑에서처럼 바꾸준다
-
-useEffect(() =>{
-  getMovies();
-}, []);
-
-  const getMovies = async () => { //async 비동기식 함수다 //변수를 붙여준다
+  getMovies = async () => { //async 비동기식 함수다
     const { //콘솔찍어보면 datat속성안에 data속성안에 movies라는 배열이 있다.
       data : {
         data : {
@@ -34,15 +26,15 @@ useEffect(() =>{
     } =
     await axios.get('https://yts-proxy.now.sh/list_movies.json?genre=animation&sort_by=like_count') //get방식은 주소창에 노출
     console.log(movies);
-    // this.setState({
-    //     isLoading: false, //false로 바꿔준다  //클래스형 컴포넌트 방식 이제는 있는 setIsLoading 함수가 isLoading: false으로 바꿔준다 
-    //     movies, //movies: movies, //속성:이름이 동일하면 하나만 써준다  //내가가져온 데이터를 넣어준다 //이제는 setMovies(movies);이렇게
-    // }) 
-    setIsLoading(false);
-    setMovies(movies); 
+    this.setState({
+        isLoading: false, //false로 바꿔준다
+        movies, //movies: movies, //속성:이름이 동일하면 하나만 써준다  //내가가져온 데이터를 넣어준다
+    })
+    
   }
 
-    // const {isLoading, movies} = this.state;//구조 분해 할당 // 더이상 필요없다 왜냐하면 usetState에서 지정한 값을 바로 갖다 쓰면 된다 
+  render() {
+    const {isLoading, movies} = this.state;//구조 분해 할당
     return (
 
         <section className='container'>
@@ -65,6 +57,7 @@ useEffect(() =>{
           }
         </section>
     )
+  }
 }
 
 export default Home;
